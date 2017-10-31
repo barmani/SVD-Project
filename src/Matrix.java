c4146caecbb1a327042dc0b051c6e9a73e5ef4f9
barmani <brendanarmani@gmail.com>
2017-10-31 18:08:52 -0400

    added initial implentations of subtract, equals, and sameDimensions methods

--- src/Matrix.java
+++ src/Matrix.java
@@ -61,7 +61,7 @@ public class Matrix {
      * @param other matrix to add to current matrix
      * @return the matrix A + B
      */
-    public Matrix addMatrix( Matrix other ) {
+    public Matrix addMatrices( Matrix other ) {
         
         Matrix sum = new Matrix( m, n );
         
@@ -79,6 +79,59 @@ public class Matrix {
         return sum;
         
     }
+    
+    /**
+     * Determine if a matrix equals another; that is, 
+     * the dimensions are the same and every entry is 
+     * the same. 
+     * 
+     * @param other the matrix to compare with 
+     * @return whether they are equal
+     */
+    public boolean equals( Matrix other ) {
+        return false;
+    }
+    
+    
+    /**
+     * Subtract two matrices. Return null if dimensions
+     * do not match.
+     * 
+     * @param other matrix to subtract from
+     * @return the matrix A - B
+     */
+    public Matrix subtractMatrices( Matrix other ) {
+        
+        Matrix difference = new Matrix( m, n );
+        
+        if ( sameDimensions( other ) ) {           
+            for ( int i = 0; i < m; i++ ) {
+                for ( int j = 0; j < n; j++ ) {                   
+                    int entry = getEntry( i, j ) - other.getEntry( i, j ); 
+                    difference.addEntry(j, i, entry);
+                }
+            }           
+        } else {     
+            
+            difference = null;
+            
+        }
+        
+        return difference;
+        
+    }
+    
+    /**
+     * Return whether the matrices have the same dimensions.
+     * 
+     * @param other matrix to compare with
+     * @return whether dimensions are the same
+     */
+    private boolean sameDimensions( Matrix other ) {
+        return m == other.getM() && n == other.getN();
+    }
+    
+    
     
     
     
