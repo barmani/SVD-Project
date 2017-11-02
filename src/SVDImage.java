b84df8d53834683dec5a783313587b727c8e47f6
Brendan Armani <brendanarmani@hotmail.com>
2017-11-02 16:50:52 -0400

    Starting SVDImage class to load image into java and get matrix of pixels

---
+++ src/SVDImage.java
@@ -0,0 +1,41 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import Jama.Matrix;
+
+import Jama.SingularValueDecomposition;
+
+public class SVDImage {
+
+    private BufferedImage image;
+    
+    private Matrix imgPixels;
+    
+    private int imgWidth;
+    private int imgHeight;
+    
+     
+    public SVDImage( String imgName ) throws IOException {
+        
+        BufferedImage image = ImageIO.read(SVDImage.class.getResource(imgName));
+        imgWidth = image.getWidth();
+        imgHeight = image.getHeight();
+        imgPixels = new Matrix( imgWidth, imgHeight );
+        
+        populateMatrixRGB();
+        
+    }
+    
+    public void populateMatrixRGB() {
+        
+        for ( int i = 0; i < imgWidth; i++ ) {
+            for ( int j = 0; j < imgHeight; j++ ) {
+                imgPixels.set( i, j, image.getRGB( i, j ) );
+            }
+        }
+        
+        imgPixels.toString();
+    }
+}
