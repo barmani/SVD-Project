69b4f2ac6a89466b7ee2bd42c789303476c03b09
barmani <brendanarmani@gmail.com>
2017-11-06 15:45:53 -0500

    Wrote comments, cleaned up code

--- src/MatrixDriver.java
+++ src/MatrixDriver.java
@@ -5,13 +5,23 @@ import Jama.Matrix;
 
 import Jama.SingularValueDecomposition;
 
+/**
+ * Driver for the SVDImage class with a test image.
+ * 
+ * @author Brendan Armani, Andrew Levy, Andrew Tomassone
+ */
 public class MatrixDriver {
 
+    /**
+     * The main method. 
+     * @param args command line arguments
+     * @throws IOException input/output exception
+     */
     public static void main(String[] args) throws IOException {
         
         SVDImage img = new SVDImage( "81.jpg" );
-        
-        
+        img.drawApproximation( 2 );
+   
     }
     
 
