7278041f947df108429833f952330a5b0c3f2905
Brendan Armani <brendanarmani@hotmail.com>
2017-11-06 15:28:53 -0500

    Try using an SVD aproximation of an image

--- src/SVDImage.java
+++ src/SVDImage.java
@@ -44,8 +44,9 @@ public class SVDImage {
         imgPixels = new Matrix( imgWidth, imgHeight );
         
         populateMatrixRGB();
-        redrawPicture();
-        
+        //redrawPicture();
+        constructSVDMatrix();
+ 
     }
     
     private void populateMatrixRGB() {
@@ -66,7 +67,7 @@ public class SVDImage {
                 image2.setRGB( i, j, (int) imgPixels.get( i, j ) );
             }
         }
-        
+
         // draw the image
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new FlowLayout());
@@ -74,6 +75,46 @@ public class SVDImage {
         frame.getContentPane().add(new JLabel(new ImageIcon( image2 )));
         frame.pack();
         frame.setVisible(true);
+        
+    }
+    
+    private void constructSVDMatrix() {
+        
+        SingularValueDecomposition svd = new SingularValueDecomposition( imgPixels );
+        Matrix u = svd.getU();
+        Matrix s = svd.getS();
+        Matrix v = svd.getV();
+        
+        Matrix newU;
+        double newS;
+        Matrix newV;
+        
+        Matrix[] pictures = new Matrix[imgWidth];
+        
+        for ( int i = 0; i < imgPixels.getColumnDimension(); i++ ) {
+            newU = u.getMatrix( 0, imgPixels.getRowDimension() - 1, i, i);
+            newV = v.getMatrix( 0, imgPixels.getColumnDimension() - 1, i, i);
+            newS = s.get( i, i );
+            Matrix matrixToAdd = newU.times(newV.transpose() );
+            matrixToAdd = matrixToAdd.times( newS );
+            
+            if ( i == 0 ) {
+                pictures[i] = matrixToAdd;
+            } else {
+                pictures[i] = pictures[i - 1].plus( matrixToAdd );
+            }
+              
+        }
+        
+        System.out.println( imgPixels.getColumnDimension() + " " + imgPixels.getRowDimension() );
+        
+        BufferedImage image2 = new BufferedImage( imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB );
+        
+        for ( int i = 0; i < imgWidth; i++ ) {
+            for ( int j = 0; j < imgHeight; j++ ) {
+                image2.setRGB( i, j, (int) pictures[359].get( i, j ) );
+            }
+        }
         
     }
   
